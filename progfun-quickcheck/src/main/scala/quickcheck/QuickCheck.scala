fe3eb453f8f79266a5912ba797a467cc70cb942e
Peixiang xu <peixiangxu@gmail.com>
2013-11-13 21:21:58 +0800

    finish first version

--- progfun-quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ progfun-quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -22,8 +22,20 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
   
-  property("isEmpty") = forAll { a: Int =>
-    val h = insert(a, empty)
-    isEmpty(h) == false
+  property("delete") = forAll { a: Int =>
+    val h = deleteMin(insert(a / 4, insert(a / 2, insert(a, empty))))
+    findMin(h) == a / 2 
+  }
+  
+  property("min2") = forAll { a: Int =>
+    val h = insert(a / 4, insert(a / 2, insert(a, empty)))
+    if (a < 0) findMin(h) == a else findMin(h) == a/4 
+  }
+  
+  property("meld") = forAll { a: Int =>
+    val h1 = insert(a / 2, insert(a, empty))
+    val h2 = insert(a / 8, insert(a / 4, empty))
+    val h = meld(h1, h2)
+    if (a < 0) findMin(h) == a else findMin(h) == a/8
   }
 }
