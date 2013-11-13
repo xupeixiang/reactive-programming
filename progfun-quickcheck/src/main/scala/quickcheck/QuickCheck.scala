48bf57d878aa1f139a2375ece6b08c8cf5f980b9
Peixiang xu <peixiangxu@gmail.com>
2013-11-13 08:59:26 +0800

    set up ok

--- progfun-quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ progfun-quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -14,8 +14,16 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(h) == a
   }
 
-  lazy val genHeap: Gen[H] = ???
+  lazy val genHeap: Gen[H] = for {
+    ele <- arbitrary[Int]
+    heap <- oneOf(value(empty), genHeap)
+  } yield insert(ele, heap)
+  
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
-
+  
+  property("isEmpty") = forAll { a: Int =>
+    val h = insert(a, empty)
+    isEmpty(h) == false
+  }
 }
